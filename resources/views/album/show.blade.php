b8a32c4686d0b5d6d2dc1dc9fe450ff7cd16d7ba
Sara <saracorsob@gmail.com>
2020-09-10 18:44:03 +0200

    Tolto dd

--- resources/views/album/show.blade.php
+++ resources/views/album/show.blade.php
@@ -3,10 +3,11 @@
   <div class="single-album">
     <h1>Titolo Album: {{$album->title}}</h1>
     <img class="album-cover" src="{{$album->image->url}}" alt="{{$album->title}}">
-    {{-- {{dd($album->artists)}} --}}
+
     @foreach ($album->artists as $artist)
       <p>Artista: {{$artist->name}}</p>
     @endforeach
+    
     <p>Anno: {{$album->year}}</p>
 
     <h2>Lista Canzoni</h2>
